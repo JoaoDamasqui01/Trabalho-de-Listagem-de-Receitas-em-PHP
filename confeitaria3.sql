3ea8ae90f8412a6cae0dc8b99982326655985641
JoaoDamasqui01 <jldamasqui2003@gmail.com>
2026-02-10 17:17:37 -0300

    pequena mudanca no script do banco

--- confeitaria3.sql
+++ confeitaria3.sql
@@ -1,29 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 27/11/2024 às 15:55
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `confeitaria3`
---
-
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `categorias`
 --
 
@@ -62,65 +37,20 @@ INSERT INTO `categorias` (`idCategorias`, `tipoCategorias`) VALUES
 --
 
 CREATE TABLE `receitas` (
-  `idReceitas` int(11) NOT NULL,
+  `idReceitas` int(11) NOT NULL PRIMARY KEY,
   `idCategorias` int(11) DEFAULT NULL,
   `titulo` varchar(40) DEFAULT NULL,
   `ingredientes` varchar(200) DEFAULT NULL,
-  `tempo_preparo` varchar(30) DEFAULT NULL
+  `tempoPreparo` varchar(30) DEFAULT NULL,
+  FOREIGN KEY (`idCategorias`) REFERENCES `categorias`(`idCategorias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `receitas`
 --
 
-INSERT INTO `receitas` (`idReceitas`, `idCategorias`, `titulo`, `ingredientes`, `tempo_preparo`) VALUES
+INSERT INTO `receitas` (`idReceitas`, `idCategorias`, `titulo`, `ingredientes`, `tempoPreparo`) VALUES
 (1, 1, 'Vinagrete', '2 cebolas grandes cortadas em cubos\r\ntomate\r\n2 tomates sem semente e cortados em cubos\r\npimentão ver', '3Minutos'),
 (2, 17, 'Macarrão', 'Meio pacote de macarrão de sua preferência;\r\n2 litros e meio de água;\r\nSal a gosto;\r\n4 colheres de sopa de óleo;\r\nMeia cebola em cubos;\r\nMeia xícara d', '1Hora');
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices de tabela `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`idCategorias`);
-
---
--- Índices de tabela `receitas`
---
-ALTER TABLE `receitas`
-  ADD PRIMARY KEY (`idReceitas`),
-  ADD KEY `idCategorias` (`idCategorias`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `idCategorias` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT de tabela `receitas`
---
-ALTER TABLE `receitas`
-  MODIFY `idReceitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `receitas`
---
-ALTER TABLE `receitas`
-  ADD CONSTRAINT `receitas_ibfk_1` FOREIGN KEY (`idCategorias`) REFERENCES `categorias` (`idCategorias`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
